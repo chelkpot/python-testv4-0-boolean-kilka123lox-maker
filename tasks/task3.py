eb00ea4d6d1e126ad60ecd6931c7cfa31ae67f38
kilka123lox-maker <kilka123lox@gmail.com>
2025-11-16 23:48:24 +0500

    aff

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,15 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
+     # Чтение двух слов
+    word1 = input().strip()
+    word2 = input().strip()
+
+# Проверка, что хотя бы одно слово равно "awesome"
+    result = word1 == "awesome" or word2 == "awesome"
+
+# Вывод результата
+    print(result)
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
